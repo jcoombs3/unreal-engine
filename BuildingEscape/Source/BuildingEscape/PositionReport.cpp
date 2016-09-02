be9d24ad00d258f9c70dab37a0ba6415eb1f6bda
pramar <jcoombs@sapient.com>
2016-09-02 01:05:49 -0400

    OpenDoor start

--- BuildingEscape/Source/BuildingEscape/PositionReport.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReport.cpp
@@ -21,7 +21,10 @@ void UPositionReport::BeginPlay()
 {
     Super::BeginPlay();
     
-    UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+    FString ObjectName = GetOwner()->GetName();
+    FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+    
+    UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 	
 }
 
